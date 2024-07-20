abd7d6f375310a0ab13c1b25b3d0bc6d6b2250c0
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-07-20 22:20:15 +0530

    done

---
+++ Core Java Projects/JavaOOPsConcept/src/inerfacedemo/InterfaceExample.java
@@ -0,0 +1,32 @@
+package inerfacedemo;
+
+interface Abc{
+	int A=10;
+	void dis1();
+}
+interface Xyz{
+	int B=20;
+	void dis2();
+}
+interface Mno extends Abc,Xyz{
+	int c=30;
+	void dis3();
+}
+class Demo implements Abc,Xyz{
+
+	@Override
+	public void dis1() {
+		System.out.println("Abc interface method");		
+	}
+	public void dis2() {
+		System.out.println("Xyz interface method");
+	}	
+}
+public class InterfaceExample {
+
+	public static void main(String[] args) {
+		Demo obj = new Demo();
+		obj.dis1();
+		obj.dis2();
+	}
+}
