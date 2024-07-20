4222afdf1c547ae0696c3dcfa4cbbb4c4bebca94
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-07-20 20:57:12 +0530

    done

---
+++ Core Java Projects/JavaOOPsConcept/src/finalkeyword/FinalKeywordExample.java
@@ -0,0 +1,32 @@
+package finalkeyword;
+
+final class Bike{
+	final void speed() {
+		System.out.println("60km/hr");
+	}
+}
+//class Honda extends Bike{
+//	@Override
+//	void speed() {
+//		System.out.println("60km/hr");
+//	}
+//}
+
+public class FinalKeywordExample {
+
+	public static void main(String[] args) {
+		final int A=10;
+		//A=20; //error
+		System.out.println(A);
+		//Honda hh=new Honda();
+		//hh.speed():
+		Bike bb=new Bike();
+		bb.speed();
+	}
+}
+
+/*
+	Output
+	10
+	60km/hr
+*/
