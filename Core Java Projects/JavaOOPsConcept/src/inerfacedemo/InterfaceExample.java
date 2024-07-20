a212fc1131fcf1b4663ce7be71910e266a5c2e25
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-07-20 22:29:28 +0530

    done

--- Core Java Projects/JavaOOPsConcept/src/inerfacedemo/InterfaceExample.java
+++ Core Java Projects/JavaOOPsConcept/src/inerfacedemo/InterfaceExample.java
@@ -1,12 +1,35 @@
 package inerfacedemo;
+/*
+ * class Abc {
+ *  int P=10;
+ * 	void dis() {
+ * 		System.out.println("Abc class dis method");
+ * 	}
+ * }
+ * class Xyz {
+ * int P=20;
+ * 	void dis() {
+ * 	System.out.println("Xyz class dis method");
+ * }
+ * }
+ * class Mno extends Abc,Xyz {			Error 
+ * 
+ * }
+ * 
+ * 
+ */
 
 interface Abc{
 	int A=10;
+	int P=100;
 	void dis1();
+	void dis();
 }
 interface Xyz{
 	int B=20;
+	int P=200;
 	void dis2();
+	void dis();
 }
 interface Mno extends Abc,Xyz{
 	int c=30;
@@ -20,8 +43,17 @@ class Demo implements Abc,Xyz{
 	}
 	public void dis2() {
 		System.out.println("Xyz interface method");
-	}	
+	}
+	@Override
+	public void dis() {
+		System.out.println("Provided body for Abc and Xyz method");
+		System.out.println("A "+A);
+		System.out.println("B "+B);
+		System.out.println("Abc interface P variable "+Abc.P);
+		System.out.println("Xyz interface P variable "+Xyz.P);
+	}
 }
+	
 public class InterfaceExample {
 
 	public static void main(String[] args) {
