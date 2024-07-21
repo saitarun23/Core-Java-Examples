0acbce5c0719d879520ffecfaac23eddc4842593
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-07-21 21:13:18 +0530

    done

--- Core Java Projects/ExceptionHandlingApp/src/com/TryWithFinally.java
+++ Core Java Projects/ExceptionHandlingApp/src/com/TryWithFinally.java
@@ -3,8 +3,28 @@ package com;
 public class TryWithFinally {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hi");
+		try {
+			int res = 100/0;
+			System.out.println("no exception");
+		} finally {
+				System.out.println("finally block");
+		}
+		System.out.println("normal statement");
 	}
-
 }
+
+/*
+	Output
+	Hi
+	int res = 100/1;
+	no exception
+	finally block
+	normal statement
+	--------------------------------------------
+	Hi
+	int res = 100/0;
+	Exception in thread "main" finally block
+	java.lang.ArithmeticException: / by zero
+		at com.TryWithFinally.main(TryWithFinally.java:8)
+*/
