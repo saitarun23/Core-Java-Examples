da96b3ee26570ec972cc79d8b2cab29706dec04f
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-07-29 11:20:19 +0530

    done

--- Core Java Projects/CollectionFrameworkApp/src/com/RetrieveCollection.java
+++ Core Java Projects/CollectionFrameworkApp/src/com/RetrieveCollection.java
@@ -13,45 +13,87 @@ public class RetrieveCollection {
 
 	public static void main(String[] args) {
 		// Set Retrieve 
-//		Set<Integer> ss1 = new HashSet<Integer>();
-//		ss1.add(10);ss1.add(20);ss1.add(40);ss1.add(30);
-//		System.out.println(ss1);
-//		System.out.println("using for each loop for set");
-//		for(int n:ss1) {
-//			System.out.println(n);
-//		}
-//		System.out.println("using iterator");
-//		Iterator<Integer> li = ss1.iterator();
-//		while(li.hasNext()) {
-//				int n = li.next();
-//				System.out.println(n);
-//		}
+/*		Set<Integer> ss1 = new HashSet<Integer>();
+		ss1.add(10);ss1.add(20);ss1.add(40);ss1.add(30);
+		System.out.println(ss1);
+		System.out.println("using for each loop for set");
+		for(int n:ss1) {
+			System.out.println(n);
+		}
+		System.out.println("using iterator");
+		Iterator<Integer> li = ss1.iterator();
+		while(li.hasNext()) {
+				int n = li.next();
+				System.out.println(n);
+		}
+		
+		Output
+		[20, 40, 10, 30]
+		using for each loop for set
+		20
+		40
+		10
+		30
+		using iterator
+		20
+		40
+		10
+		30
+*/	
 		
 		//List Retrieve
-//		List<Integer> ll=new ArrayList<Integer>();
-//		ll.add(10);ll.add(20);ll.add(30);ll.add(40);ll.add(50);
-//		System.out.println(ll);
-//		System.out.println("for each");
-//		for(int n:ll) {
-//			System.out.println(n);
-//		}
-//		System.out.println("using iterator");
-//		Iterator<Integer> li = ll.iterator();
-//		while(li.hasNext()) {
-//			int n = li.next();
-//			System.out.println(n);
-//		}
-//		System.out.println("using list iterator - forward direction");
-//		ListIterator<Integer> li1=ll.listIterator();
-//		while(li1.hasNext()) {
-//			int n = li1.next();
-//			System.out.println(n);
-//		}
-//		System.out.println("using list iterator - backward direction");
-//		while(li1.hasPrevious()) {
-//			int n = li1.previous();
-//			System.out.println(n);
-//		}
+/*		List<Integer> ll=new ArrayList<Integer>();
+		ll.add(10);ll.add(20);ll.add(30);ll.add(40);ll.add(50);
+		System.out.println(ll);
+		System.out.println("for each");
+		for(int n:ll) {
+			System.out.println(n);
+		}
+		System.out.println("using iterator");
+		Iterator<Integer> li = ll.iterator();
+		while(li.hasNext()) {
+			int n = li.next();
+			System.out.println(n);
+		}
+		System.out.println("using list iterator - forward direction");
+		ListIterator<Integer> li1=ll.listIterator();
+		while(li1.hasNext()) {
+			int n = li1.next();
+			System.out.println(n);
+		}
+		System.out.println("using list iterator - backward direction");
+		while(li1.hasPrevious()) {
+			int n = li1.previous();
+			System.out.println(n);
+		}
+		
+		Output
+		[10, 20, 30, 40, 50]
+		for each
+		10
+		20
+		30
+		40
+		50
+		using iterator
+		10
+		20
+		30
+		40
+		50
+		using list iterator - forward direction
+		10
+		20
+		30
+		40
+		50
+		using list iterator - backward direction
+		50
+		40
+		30
+		20
+		10
+*/
 		
 		//Map Retrieve
 		Map<Integer, String> mm=new HashMap<Integer, String>();
@@ -67,5 +109,12 @@ public class RetrieveCollection {
 			System.out.println("key "+k+" value "+mm.get(k));
 		}
 		System.out.println(mm);
+		
+/*
+		Output{1=Steven, 2=Ravi}
+		key 1 value null
+		key 2 value Ravi
+		{2=Ravi}
+*/
 	}
 }
