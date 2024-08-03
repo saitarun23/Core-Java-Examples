9f3636762b3bbf00ad086df08694555f35b0d8cc
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-08-03 23:07:44 +0530

    done

--- Core Java Projects/JdbcTestApp/src/com/JdbcTestDemo.java
+++ Core Java Projects/JdbcTestApp/src/com/JdbcTestDemo.java
@@ -9,6 +9,14 @@ public class JdbcTestDemo {
 			System.out.println("Driver loaded successfully");
 			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/my_db","root","saivarun");
 			System.out.println("Connected Successfuly");
+			Statement stmt=con.createStatement();
+			
+			int result=stmt.executeUpdate("insert into employee values(5,'neena',27000)");
+			if(result>0) {
+				System.out.println("Record inserted successfully");
+			}
+			stmt.close();
+			con.close();
 		}catch(Exception e) {
 			System.err.println(e);
 		}
