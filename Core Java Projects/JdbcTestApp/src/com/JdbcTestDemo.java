faf1026df38a57c6997d4bad2d72330e9c79f495
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-08-03 22:26:04 +0530

    done

---
+++ Core Java Projects/JdbcTestApp/src/com/JdbcTestDemo.java
@@ -0,0 +1,15 @@
+package com;
+import java.sql.*;
+
+public class JdbcTestDemo {
+
+	public static void main(String[] args) {
+		try {
+			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+
+	}
+
+}
