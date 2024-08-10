eeb8c45fd44738a485ff01bb3baa8ac0f69f0e3a
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-08-10 22:17:43 +0530

    done

--- JavaEE Workspace/SimpleWebApp/src/main/java/com/LoginServlet.java
+++ JavaEE Workspace/SimpleWebApp/src/main/java/com/LoginServlet.java
@@ -1,31 +1,26 @@
 package com;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LoginServlet
- */
+
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+   
     public LoginServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter pw=response.getWriter();
 	}
 
 	/**
