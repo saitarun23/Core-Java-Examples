32a31e812d984c1a7467121ae20f873e5121e084
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-08-17 23:11:00 +0530

    done

--- JavaEE Workspace/Product_Management_System_App/src/main/java/com/pms/resource/DbResource.java
+++ JavaEE Workspace/Product_Management_System_App/src/main/java/com/pms/resource/DbResource.java
@@ -5,17 +5,25 @@ import java.sql.DriverManager;
 
 public class DbResource {
 	
-	
+	static Connection con;
 	public static Connection getDbConnection() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/my_db", "root", "saivarun");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/my_db", "root", "saivarun");
 			return con;
 			
 		}catch(Exception e) {
 			System.err.println(e);
 		}
 		return null;
+	}
+	
+	public static void close() {
+		try {
+			
+		} catch (Exception e) {
+			System.err.println(e);
+		}
 	}
 
 }
