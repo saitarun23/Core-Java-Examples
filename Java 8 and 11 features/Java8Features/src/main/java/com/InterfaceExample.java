9441f87d90ef00ff1746e87103b5adba095b033a
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-08-24 20:03:26 +0530

    done

---
+++ Java 8 and 11 features/Java8Features/src/main/java/com/InterfaceExample.java
@@ -0,0 +1,40 @@
+package com;
+interface Abc{
+	void dis1();
+	default void dis2() {
+		System.out.println("dis2 default method");
+	}
+	default void dis3() {
+		System.out.println("dis3 default method");
+	}
+	static void dis4() {
+		System.out.println("dis4 static method");
+	}
+}
+
+class Test implements Abc{
+	@Override
+	public void dis1() {
+		System.out.println("Test class provided body for dis1 method");
+	}
+	@Override
+	public void dis2() {
+		System.out.println("Test class default method override");
+	}
+//	@Override
+//	static void dis4() {
+//		System.out.println("dis4 static method");
+//	}
+}
+
+public class InterfaceExample {
+	
+	public static void main(String[] args) {
+		Test t1 = new Test();
+		t1.dis1();
+		t1.dis2();
+		t1.dis3();
+		Abc.dis4();    // 	calling using interface name
+	}
+
+}
