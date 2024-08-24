daf7c731ace2b6167466435fb1878c8c309395c4
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-08-24 20:28:27 +0530

    done

--- Java 8 and 11 features/Java8Features/src/main/java/com/LambdaExpressionExample.java
+++ Java 8 and 11 features/Java8Features/src/main/java/com/LambdaExpressionExample.java
@@ -12,11 +12,24 @@ class HelloImp implements Hello{
 public class LambdaExpressionExample {
 
 	public static void main(String[] args) {
-		//
+		
+		//1st option to provide the body for Hello interface
 		Hello obj1=new HelloImp();
 		System.out.println(obj1.sayHello());
-		//
 		
+		//2nd option to provide the body for Hello interface using anonymous inner class.
+		
+		Hello obj2=new Hello() {
+			@Override
+			public String sayHello() {
+				hello();
+				return "Welcome using Anonymous inner class";
+			}
+			public void hello() {
+				System.out.println("Hello method of anonymous class");
+			}
+		};
+		System.out.println(obj2.sayHello());
 	}
 
 }
