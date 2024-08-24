bf30730e67de99f3366fc4f32e70deb80ceb9a70
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-08-24 23:05:21 +0530

    done

--- Java 8 and 11 features/Java8Features/src/main/java/com/StreamAPIExample1.java
+++ Java 8 and 11 features/Java8Features/src/main/java/com/StreamAPIExample1.java
@@ -1,7 +1,9 @@
 package com;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class StreamAPIExample1 {
 
@@ -29,5 +31,24 @@ public class StreamAPIExample1 {
 		System.out.println(result1);
 		boolean result2 = names.stream().allMatch(v->v.contains("a"));
 		System.out.println(result2);
+		
+//		List<String> names = new ArrayList<String>();
+//		names.add("Ravi");names.add("Ajay");names.add("Stevena");names.add("Mahesh");names.add("Johna");
+//		names.add("Raju");names.add("Vijay");names.add("Reeta");names.add("Leena");names.add("Ramu");
+		
+//		Stream<String> ss=names.stream();
+//		Stream<String> ss1 = ss.filter(p->p.contains("ee"));
+//		ss1.forEach(s->System.out.println(s));
+//		//ss.forEach(s->System.out.println(s));
+		
+		List<Integer> ll = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+		int sum = ll.stream().reduce(0, (ival,curval)->ival+curval);
+		int sumofeven = ll.stream().filter(f->f%2==0).reduce(0, (ival,curval)->ival+curval);
+//		int sum = ll.stream().reduce(0, (ival,curval)->{
+//			System.out.println("ival "+ival+" cval "+curval);
+//			return ival+curval;
+//		});
+		System.out.println(sum);
+		System.out.println(sumofeven);
 	}
 }
